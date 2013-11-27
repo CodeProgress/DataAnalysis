ec705700747a8863f4863dd195274ba6ca605d65
codeprogress <codeprogress1@gmail.com>
2013-11-27 18:23:32 -0500

    Commit snakesAndLaddersSim.py
    The following program is a simulation of Snakes & Ladders.
    
    The final product is a heat map that shows the relative frequency of
    landing on a given square.

---
+++ snakesAndLaddersSim.py
@@ -0,0 +1,140 @@
+
+'''
+The following program is a simulation of Snakes & Ladders
+The final product is a heat map that shows the relative frequency of landing on
+a given square.
+
+'''
+
+import random
+import pylab
+import collections
+
+def climb_or_slide(currentPosition, 
+                    snakes  = {16:  6,     47: 26,     49: 11,
+                               56: 53,     62: 19,     64: 60, 
+                               87: 24,     93: 73,     95: 75, 
+                               98: 78},
+
+                    ladders = { 1: 38,      4: 14,      9: 31, 
+                               21: 42,     28: 84,     36: 44, 
+                               51: 67,     71: 91,     80: 100}
+                    ):
+    '''if current position is on a magic square, return new location
+    else return the current position
+    '''
+    if currentPosition in snakes:
+        return snakes[currentPosition]
+
+    if currentPosition in ladders:
+        return ladders[currentPosition]
+
+    return currentPosition
+
+def roll(numSides = 6):
+    ''' returns a random int between 1 and numSides, inclusive.
+    '''
+    #return random.randint(1, numSides) #slow
+    return int(random.random() * numSides) + 1
+
+
+def play_game(boardSize = 100, currentPosition = 0):
+    '''Plays through an entire game, returns an ordered list of all squares
+    that were landed on
+    '''
+    positions = []
+    #do not have to land on 100 exactly, if so, landing position spread too big
+    while currentPosition < boardSize:
+        currentPosition = climb_or_slide(currentPosition + roll())
+        positions.append(currentPosition)
+    return positions
+
+def rel_freq_landings(numGames):
+    """returns the relative frequency of landing on each square over numGames.
+    relative frequency = (num times landed on sq)/(num times landed on all sqs)
+    sum(rel_freq_landings(numGames)) ~ .985 since some games end past goal sq
+    """
+    landings = collections.Counter()
+    count = 0.0
+    for i in xrange(numGames):
+        for j in play_game():
+            landings[j] += 1
+            count += 1
+    return [landings[k]/count for k in xrange(0,101)]
+
+def make_array(landingList, startVal = 100, rowSize = 10):
+    val = startVal
+    asArray = []
+    desc = False #descending if False, ascending if True
+    while val > 0:
+        row = []
+        counter = rowSize
+        while counter > 0:
+            row.append(landingList[val])
+            val -= 1
+            counter -= 1
+        if desc:
+            row.reverse()
+        desc = not desc
+        asArray.append(row)
+    asArray.reverse() #to match the gameboard visually
+    return asArray
+
+def label_board(arrayA):
+    ''' label each square corresponding to 10x10 gameboard layout
+    numbers start in bottom left, go right and follow a boustrophedon path
+    to top left.  (the way an ox would plow a field.)
+    '''
+    for i in range(10):
+        for j in range(10):
+            if i % 2 ==0:
+                if j == 9:
+                    ones = str(0)
+                    tens = str(i+1)
+                else: 
+                    ones = str(j+1)
+                    tens = str(i)
+            else:
+                if j == 0:
+                    ones = str(0)
+                    tens = str(i+1)
+                else:
+                    ones = str(10-j)
+                    tens = str(i)
+
+            pylab.text(j+0.45, i+0.45, tens + ones, 
+                        ha='center', va='bottom',color='w')
+        
+def plot_heat(landings):
+    
+    data = pylab.array(landings)
+
+    #remove any values that equal zero
+    data = pylab.ma.masked_equal(data, 0)
+    
+    fig, ax = pylab.subplots()
+    im = ax.pcolormesh(data)
+    fig.colorbar(im, label = "Relative Frequency (%)")
+    
+    #set the background to hatched so removed values appear hatched
+    p = pylab.Rectangle((0,0), 10, 10, hatch = 'xx', fill = None, zorder = -1)
+    ax.add_patch(p)
+    
+    pylab.axis('off')
+    pylab.title('Snakes and Ladders Heat Map for All Game Squares')
+
+    label_board(data)
+
+    pylab.show()
+
+def run_cProfile(numGames = 10000):
+    import cProfile
+    cProfile.run('make_array(rel_freq_landings(' + str(numGames) + '))')
+
+
+if __name__ == "__main__":
+    numGames = 10000
+    plot_heat(make_array(rel_freq_landings(numGames)))
+
+
+
