159422caef089241d98a982c20ee4d8b99b65afc
codeprogress <codeprogress1@gmail.com>
2014-04-03 13:30:49 -0400

    Commit harmonicNumbersGraph.py

---
+++ harmonicNumbersGraph.py
@@ -0,0 +1,22 @@
+import pylab
+import math
+
+harmSumUntil = {0:0}
+    
+
+def harm(x):
+    if x in harmSumUntil:
+        return harmSumUntil[x]
+    harmSumUntil[x] = harm(x-1) + 1./x
+    return harmSumUntil[x]
+    
+    #Non-recursive and non-memoized form: sum([1./y for y in range(1, x+1)])
+    
+
+pylab.plot([harm(x) for x in range(1, 10000)], label="harmonic")
+pylab.plot([math.log(x) for x in range(1, 10000)], label="log")
+pylab.plot([x**(1/3.) for x in range(1, 10000)], label="**(1/.3)")
+
+pylab.legend()
+pylab.show()
+
