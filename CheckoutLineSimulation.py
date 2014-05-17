dad1b8c9c358c1280539e40888f667be645960f7
codeprogress <codeprogress1@gmail.com>
2014-05-16 23:56:44 -0400

    Update CheckoutLineSimulation.py

--- CheckoutLineSimulation.py
+++ CheckoutLineSimulation.py
@@ -21,37 +21,55 @@ when they choose optimally, choose shortest line, choose randomly?
 
 import random
 import collections
+import Queue
 
 class CheckoutPoint(object):
-    def __init__(self, itemsPerMinute):
-        self.itemsPerMinute = itemsPerMinute
-        self.itemsPerSecond = itemsPerMinute/60.     
-                
+    def __init__(self, itemsPerMinute = 10):
+        self.itemsPerMinute  = itemsPerMinute
+        self.itemsPerSecond  = itemsPerMinute/60.
+        self.currentCustomer = None
+        self.line            = collections.deque()
+        
+    def start_checking_out_customer(self, customer):
+        self.currentCustomer = customer
+    
+    def is_occupied(self):
+        if self.currentCustomer.numItems <= 0:
+            self.currentCustomer = None 
+            return True
+        self.currentCustomer.numItems -= self.itemsPerSecond
+        return False
                                 
 class Store(object):
     def __init__(self):
         self.customers      = []
-        self.checkOutPoints = {}        #key: CheckoutPoint object, value: Queue
+        self.checkoutPoints = Queue.PriorityQueue()   #(priority_number, data)
     
     def acquire_customer(self, customer):
         self.customers.append(customer)
     
-    def open_checkout_point(self, checkoutPoint):
-        self.checkOutPoints[checkoutPoint] = collections.deque
-        
+    def open_checkout_point(self):
+        newRegister = (0, CheckoutPoint())
+        self.checkoutPoints.put(newRegister)
         
 class Customer(object):
     def __init__(self, numItems = None):
-        if numItems == None: self.numItems = max(1, random.gauss(30, 10))
-        else: self.numItems = numItems
-
+        if numItems == None: 
+            self.numItems = max(1, random.gauss(30, 10))
+        else: 
+            self.numItems = numItems
+        
         self.timeInLine      = 0
         self.timeCheckingOut = 0
         self.totalTime       = 0
     
+        
+    
+    
 store = Store()
      
-register = CheckoutPoint(10)
+register = CheckoutPoint()
 
-store.open_checkout_point(register)
+store.open_checkout_point()
         
+print store.checkoutPoints.get()
