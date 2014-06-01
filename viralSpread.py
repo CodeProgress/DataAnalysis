6db9943c73b81328d856fb1e487eb4a852807ae6
codeprogress <codeprogress1@gmail.com>
2014-05-31 20:23:33 -0400

    Update viralSpread.py

--- viralSpread.py
+++ viralSpread.py
@@ -1,11 +1,18 @@
 
 import random
+import pylab
 
 
 def simulate_viral_spread(numPeople, 
                           infectionRate, 
                           numEncounters, 
-                          numStartingWithVirus):
+                          numStartingWithVirus,
+                          verbose = False):
+    """
+    Returns two values:
+        the percentage of the population starting with the virus
+        the percentage ending with the virus
+    """
     population = [0 for i in range(numPeople)]
     
     for j in random.sample(xrange(numPeople), numStartingWithVirus):
@@ -20,11 +27,14 @@ def simulate_viral_spread(numPeople,
                 population[y] = 1
     
     startingPercent  = numStartingWithVirus/float(numPeople)*100
-    percentWithVirus = sum(population)/float(numPeople) * 100
-    print '{} percent of the population started with the virus'.format(startingPercent)
-    print '{} percent of the population is infected'.format(percentWithVirus)   
+    endingPercent    = sum(population)/float(numPeople) * 100
+    if verbose:
+        print '{} percent of the population started with the virus'.format(startingPercent)
+        print '{} percent of the population is infected'.format(endingPercent)   
+    return startingPercent, endingPercent
+
 
 print simulate_viral_spread(numPeople           = 10000, 
-                            infectionRate        = .2, 
-                            numEncounters        = 10000,
-                            numStartingWithVirus = 100)
+                            infectionRate        = .05, 
+                            numEncounters        = 50000,
+                            numStartingWithVirus = 1000)
