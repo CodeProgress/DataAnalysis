734d5239d9001c501d46268b4810ba9bc93e8942
codeprogress <codeprogress1@gmail.com>
2015-10-19 20:42:38 -0400

    Refactor and add plot of chance within N points

--- optimalStopping.py
+++ optimalStopping.py
@@ -1,8 +1,7 @@
 #optimal stopping
 
-import random
 import pylab
-
+import numpy
 
 def pick_val(values, stoppingPoint):
     """Strategy: The Cutoff Rule
@@ -14,14 +13,36 @@ def pick_val(values, stoppingPoint):
     values: List, a continue range of numbers, ex: range(100)
     stoppingPoint: Integer between min(values) and max(values), inclusive
     """
-    best = values[0]
-    for i in range(len(values)):
-        if i < stoppingPoint:
-            if values[i] > best:
-                best = values[i]
+    bestSoFar = values[0]
+    for i, value in enumerate(values):
+        if i < stoppingPoint and value > bestSoFar:
+            bestSoFar = value
         else:
-            if values[i] > best:
-                return values[i]
+            if value > bestSoFar:
+                return value
+    return values[-1]
+
+def pick_val_after_2_new_bests(values, stoppingPoint):
+    return pick_val_after_N_new_bests(values, stoppingPoint, 2)
+
+def pick_val_after_3_new_bests(values, stoppingPoint):
+    return pick_val_after_N_new_bests(values, stoppingPoint, 3)
+
+def pick_val_after_N_new_bests(values, stoppingPoint, numNewBestsToStopAfter):
+    """
+    Same as pick_val except when higher number is found, use it as the new best
+    and keep iterating until you find something higher.
+    """
+    bestsSoFar = [values[0]]
+    for i, value in enumerate(values):
+        if i < stoppingPoint and value > bestsSoFar[0]:
+            bestsSoFar[0] = value
+        else:
+            if value > bestsSoFar[-1]:
+                if len(bestsSoFar) >= numNewBestsToStopAfter:
+                    return value
+                else:
+                    bestsSoFar.append(value)
     return values[-1]
 
 def pick_ccr_val(values, numCandidates):
@@ -36,14 +57,13 @@ def pick_ccr_val(values, numCandidates):
             if numCandsSeen == numCandidates:
                 return values[i]                
 
-    return values[-1]
-        
+    return values[-1]        
 
 def sim_outcome(numVals, stoppingPoint, numTrials, strategyFunction):
     """returns the average result over numTrials"""
     total = 0.
     for i in range(numTrials):
-        values = random.sample(xrange(numVals), numVals)
+        values = numpy.random.choice(10, 10, replace=False)
         #values = [random.randint(0, numVals-1) for x in range(numVals)]
         total += strategyFunction(values, stoppingPoint)
     return total/numTrials 
@@ -52,7 +72,7 @@ def sim_outcome_list(numVals, stoppingPoint, numTrials, strategyFunction):
     """returns a list of the chosen values when stopping point reached"""
     total = []
     for i in range(numTrials):
-        values = random.sample(xrange(numVals), numVals)
+        values = numpy.random.choice(numVals, numVals, replace=False)
         #values = [random.randint(0, numVals-1) for x in range(numVals)]
         total.append(strategyFunction(values, stoppingPoint))
     return total 
@@ -65,16 +85,42 @@ def plot_average_outcome(numVals, numTrials, strategyFunction):
 
 def plot_chance_of_optimal(numVals, numTrials, strategyFunction):
     results = []
-    for x in range(numVals):
-        outcomes = sim_outcome_list(numVals, x, numTrials, strategyFunction)    
-        results.append(sum(1 for x in outcomes if x == numVals -1)/float(numTrials))
+    optimalVal = numVals-1 
+    for stoppingPoint in range(numVals):
+        outcomes = sim_outcome_list(numVals, stoppingPoint, numTrials, strategyFunction)   
+        results.append(sum(1 for chosenVal in outcomes if chosenVal == optimalVal)/float(numTrials))
     pylab.plot(results)
     pylab.title("Likelihood of Stopping Optimally")
     pylab.xlabel("Stopping Points")
     pylab.ylabel("Percent (as decimal between 0 and 1)")
     pylab.xticks(range(0,numVals, 5))
 
-plot_chance_of_optimal(100,500, pick_val)
+def plot_chance_of_chosen_being_within_N_points_of_optimal(numVals, numTrials, strategyFunction, NPoints):
+    results = []
+    optimalVal = numVals-1 
+    for stoppingPoint in range(numVals):
+        outcomes = sim_outcome_list(numVals, stoppingPoint, numTrials, strategyFunction)   
+        results.append(sum(1 for chosenVal in outcomes if abs(chosenVal-optimalVal) <= NPoints)/float(numTrials))
+    pylab.plot(results, label="Within {}".format(NPoints))
+    pylab.title("Likelihood of Stopping Witin N of Optimal")
+    pylab.xlabel("Stopping Points")
+    pylab.ylabel("Percent (as decimal between 0 and 1)")
+    pylab.xticks(range(0,numVals, 5))
+
+#plot_chance_of_optimal(100,500, pick_val)
+#plot_chance_of_optimal(100,500, pick_val_after_2_new_bests)
+#plot_chance_of_optimal(100,500, pick_val_after_3_new_bests)
 #plot_chance_of_optimal(100,500, pick_ccr_val)
-#plot_average_outcome(100,500)
+
+#plot_average_outcome(100,500, pick_val)
+#plot_average_outcome(100,500, pick_val_after_2_new_bests)
+#plot_average_outcome(100,500, pick_val_after_3_new_bests)
+
+plot_chance_of_chosen_being_within_N_points_of_optimal(100,300, pick_val, 0)
+plot_chance_of_chosen_being_within_N_points_of_optimal(100,300, pick_val, 1)
+plot_chance_of_chosen_being_within_N_points_of_optimal(100,300, pick_val, 2)
+plot_chance_of_chosen_being_within_N_points_of_optimal(100,300, pick_val, 3)
+plot_chance_of_chosen_being_within_N_points_of_optimal(100,300, pick_val, 10)
+pylab.legend()
+
 pylab.show()
