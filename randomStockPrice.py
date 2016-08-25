31189d99b6b83cb937da6195a0043228f94f2a7f
codeprogress <codeprogress1@gmail.com>
2016-08-24 23:58:49 -0400

    Rewrite randomStockPrice.py

--- randomStockPrice.py
+++ randomStockPrice.py
@@ -1,25 +1,30 @@
 import pylab
 import random
 
-num_positive = []
-for step in range(20):
-    count = 0
-    offset = 1 - (step * 3.5**-5)
-    print offset
-    for trial in xrange(100):
-        # data = []
-        price = 0
-        price = sum(random.random() * 2 - offset for _ in xrange(10000))
-        # for i in xrange(10000):
-        #     price += random.random()*2 - .9999
-            # data.append(price)
-        if price > 0:
-            count += 1
-    num_positive.append(count)
 
-print count
-pylab.plot(num_positive)
+def simulated_stock_prices(num_iterations):
+    assert num_iterations > 0
+    prices = []
+    price = 0
+    for i in xrange(num_iterations):
+        change_in_price = random.random() * 2 - 1
+        price += change_in_price
+        prices.append(price)
+    return prices
+
+
+def final_stock_price_after_simulation(prices):
+    return prices[-1]
+
+
+def multi_trial_stock_prices(num_trials, num_iterations_in_trial):
+    assert num_trials > 1
+    all_prices = []
+    for trial in xrange(num_trials):
+        all_prices.append(final_stock_price_after_simulation(simulated_stock_prices(num_iterations_in_trial)))
+    return all_prices
+
+simulated_prices = simulated_stock_prices(100)
+
+pylab.plot(simulated_prices)
 pylab.show()
-
-# pylab.plot(data)
-# pylab.show()
