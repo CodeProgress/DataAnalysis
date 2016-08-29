705a072368368e350977312e0c91526af8de5d8d
codeprogress <codeprogress1@gmail.com>
2016-08-28 20:59:37 -0400

    Add more distributions

--- randomArrayWithBiasDistributionHeatMaps.py
+++ randomArrayWithBiasDistributionHeatMaps.py
@@ -26,12 +26,39 @@ def bias_distribution_row_vals_high_to_low(outcome_array):
             if startIndex == tailIndex:
                 break
     return biased_array
+    
+def shuffle_bias_lower_values_at_beginning_of_array(outcome_array):
+    # compare val to random element in array, if lower, swap with that value
+    biased_array = outcome_array.copy()
+    rows, cols = biased_array.shape
+    for row in xrange(rows):
+        for col in xrange(cols):
+            randRow = random.randint(0, rows-1)
+            randCol = random.randint(0, cols-1)
+            if biased_array[row][col] < biased_array[randRow][randCol]:
+                biased_array[row][col], biased_array[randRow][randCol] \
+                    = biased_array[randRow][randCol], biased_array[row][col]
+    return biased_array
 
+def shuffle_bias_lower_values_at_beginning_of_array_subtle(outcome_array):
+    # compare val to random.random(), if lower, swap with random element of array
+    biased_array = outcome_array.copy()
+    rows, cols = biased_array.shape
+    for row in xrange(rows):
+        for col in xrange(cols):
+            randRow = random.randint(0, rows-1)
+            randCol = random.randint(0, cols-1)
+            if biased_array[row][col] < random.random():
+                biased_array[row][col], biased_array[randRow][randCol] \
+                    = biased_array[randRow][randCol], biased_array[row][col]
+    return biased_array
+
+bias_function = shuffle_bias_lower_values_at_beginning_of_array_subtle
 rows = 200
 cols = 200
 
 random_array = pylab.rand(rows,cols)
 
-pylab.pcolor(bias_distribution_row_vals_high_to_low(random_array))
+pylab.pcolor(bias_function(random_array))
 pylab.colorbar()
 pylab.show()
