3e9040bf97105796ad557bcf1ca4c1f3b01a9187
codeprogress <codeprogress1@gmail.com>
2016-09-03 20:16:27 -0400

    Refactor and add more random walk functions

--- randomWalkExamples.py
+++ randomWalkExamples.py
@@ -2,17 +2,59 @@ import pylab
 import random
 
 
-def random_with_exponent(num_trials):
-    xlist = [random.random()**.5 for i in range(num_trials)]
-    ylist = [random.random()**.5 for j in range(num_trials)]
+def random_walk(num_trials):
+    xlist = [0]
+    ylist = [0]
+    xval = 0
+    yval = 0
+    for i in range(num_trials):
+        xval += random.randint(-1, 1)
+        yval += random.randint(-1, 1)
+        xlist.append(xval)
+        ylist.append(yval)
+
     return xlist, ylist
-    
+
+
+def random_walk_continuous(num_trials):
+    xlist = [0]
+    ylist = [0]
+    xval = 0
+    yval = 0
+    for i in range(num_trials):
+        xval += random.uniform(-1, 1)
+        yval += random.uniform(-1, 1)
+        xlist.append(xval)
+        ylist.append(yval)
+
+    return xlist, ylist
+
+
+def random_walk_continuous_with_random_pos_and_neg(num_trials):
+    '''aka the "light speed ahead" random walk
+    set num_trials to 1000 for best result'''
+    xlist = [0]
+    ylist = [0]
+    xval = 0
+    yval = 0
+    for i in range(num_trials):
+        xval += random.random()
+        yval += random.random()
+        if random.random() < .5:
+            xval = -xval
+        if random.random() < .5:
+            yval = -yval
+        xlist.append(xval)
+        ylist.append(yval)
+
+    return xlist, ylist
+
 
 def random_unique_x_and_y_coords(num_trials):
     xvisited = set()
     yvisited = set()
-    xlist = []
-    ylist = []
+    xlist = [0]
+    ylist = [0]
     xval = 0
     yval = 0
     for i in range(num_trials):
@@ -34,8 +76,8 @@ def random_unique_x_and_y_coords(num_trials):
 
 def random_unique_xy_coords(num_trials):
     visited = set()
-    xlist = []
-    ylist = []
+    xlist = [0]
+    ylist = [0]
     xval = 0
     yval = 0
     for i in range(num_trials):
@@ -49,5 +91,15 @@ def random_unique_xy_coords(num_trials):
     return xlist, ylist
 
 
-pylab.scatter(*random_unique_xy_coords(1000))
-pylab.show()
+def plot_random_walk_function(random_walk_function, num_trials=1000):
+    xvals, yvals = random_walk_function(num_trials)
+    pylab.plot(xvals, yvals, '-o')
+    
+    pylab.plot(0, 0, color='green', marker='s')                # start point
+    pylab.plot(xvals[-1], yvals[-1], color='red', marker='8')  # end point
+    pylab.plot([0, xvals[-1]], [0, yvals[-1]], color='red')
+    
+    pylab.show()
+
+
+plot_random_walk_function(random_walk)
