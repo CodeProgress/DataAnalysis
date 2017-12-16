026ee06a10f433efa89aa1f2aa68a388ec5eb59e
CodeProgress <codeprogress1@gmail.com>
2017-12-15 17:55:46 -0700

    Rename variables and sketch numpy speedup

--- randomWalkCollisionsMulti.py
+++ randomWalkCollisionsMulti.py
@@ -2,6 +2,7 @@ import pylab
 import random
 import collections
 
+
 def get_location_after_random_step(current_location, low_bound, high_bound):
     x, y = current_location
     x += random.randint(-1, 1)
@@ -11,10 +12,12 @@ def get_location_after_random_step(current_location, low_bound, high_bound):
     
     return x, y
     
+    
 def bounded_location(x, y, low_bound, high_bound):
     x = bound(x, low_bound, high_bound)
     y = bound(y, low_bound, high_bound)
     return x, y
+    
     
 def bound(dimension, low_bound, high_bound):
     if dimension < low_bound: 
@@ -23,19 +26,23 @@ def bound(dimension, low_bound, high_bound):
         dimension = high_bound
     return dimension
 
+
 def get_new_locations_after_all_people_randomly_step(people, low_bound, high_bound):
     new_people = []
     for person in people:
         new_people.append(get_location_after_random_step(person, low_bound, high_bound))
     return new_people
     
+
 def rem_one_of_duplicates(people):
     return list(set(people))
+
 
 def rem_all_duplicates(people):
     hist = collections.Counter(people)
     seen_once = [x for x in hist if hist[x] ==1]
     return seen_once
+    
     
 def get_random_starting_locations(num_people, low_bound, high_bound):
     people = []
@@ -48,10 +55,10 @@ def get_random_starting_locations(num_people, low_bound, high_bound):
 
 def plot_remaining_people_after_steps_until_target(
         starting_num_people,
+        low_bound = -20,
+        high_bound = 20,
         target_remaining = 5):
     
-    low_bound = -20
-    high_bound = 20
     people = get_random_starting_locations(
                 starting_num_people,
                 low_bound,
@@ -61,24 +68,24 @@ def plot_remaining_people_after_steps_until_target(
     #pylab.show()
     
     size = len(people)
-    counter = 0
+    step_counter = 0
     sizes = [size]
-    counters = [counter]
-    # print size, counter
-    
+    step_history_after_collision = [step_counter]
+
+    # everyone walks around room until target num remaining
     while size > target_remaining:
-        counter += 1
+        step_counter += 1
         #people = rem_one_of_duplicates(people)
         people = rem_all_duplicates(people) 
         people = get_new_locations_after_all_people_randomly_step(
                     people, low_bound, high_bound)
         if len(people) < size:
             sizes.append(size)
-            counters.append(counter)
+            step_history_after_collision.append(step_counter)
             size = len(people)
-            # print size, counter
     
-    pylab.plot(counters, sizes, label=starting_num_people)
+    pylab.plot(step_history_after_collision, sizes, label=starting_num_people)
+
 
 for starting_num_people in range(10, 200, 20):
     plot_remaining_people_after_steps_until_target(starting_num_people)
@@ -89,3 +96,11 @@ pylab.ylabel("Number of people remaining in room")
 pylab.title("Number of random steps needed until all people collide")
 pylab.show()
 
+
+#cProfile.run('profile_plot()', sort='cumulative')
+
+# speedup using numpy
+# x = numpy.array([0]*10)
+# x += numpy.random.randint(-1, 2, 10)
+# x[x>10] = 10
+# x[x<-10] = 10
