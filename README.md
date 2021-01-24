3958cf37aec477c4ff2b46c84c27413c6416f5d7
CodeProgress <CodeProgress1@gmail.com>
2021-01-23 19:42:55 -0700

    Update ReadMe, add matplotlib screenshots, reformat

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 DataAnalysis
 ============
-This repository is a collection of data analysis projects.
 
-Contents:
--Plot of chess ratings for Grandmasters rated 2700+.  (Data from http://www.2700chess.com/)
--Heat map showing the relative frequency of landing on a given square in the board game Snakes and Ladders
+###Examples:
+
+- **Heat map** showing the relative frequency of landing on a given square in the board game Snakes and Ladders. 
+  *snakesAndLaddersSim.py*
+![img.png](snakes_and_ladders_heat_map.png)
+  
+
+- **Monte Carlo Simulation** of the likelihood that the better team wins a series of X games if we know the team's chances 
+of winning a single game. *plotWorldSeriesSim.py*
+![img.png](win_rate_vs_num_games_in_series.png)
--- plotWorldSeriesSim.py
+++ plotWorldSeriesSim.py
@@ -5,15 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_win_rate_vs_num_games_in_series(
-        max_number_of_games_in_series,
-        chance_better_team_wins=.60,
-        num_trials=100):
+def plot_win_rate_vs_num_games_in_series(max_number_of_games_in_series, chance_better_team_wins=.60, num_trials=100):
     """Plots the win rate for the better team based on the number games in the series.
     Estimates win rates based on Monte Carlo simulation.
-    Plotted number of games in series will be odd numbers between 3 and max_number_of_games_in_series."""
+    Plotted number of games in series will be odd numbers between 3 and max_number_of_games_in_series.
+    """
     series_win_rates = []
     list_of_num_games_in_series = range(3, max_number_of_games_in_series, 2)
+
     for num_games_in_series in list_of_num_games_in_series:
         series_win_rate = simulate_world_series(chance_better_team_wins, num_games_in_series, num_trials)
         series_win_rates.append(series_win_rate)
@@ -28,12 +27,15 @@ def plot_win_rate_vs_num_games_in_series(
 
 def simulate_world_series(chance_better_team_wins, num_games_in_series, num_trials):
     num_series_won_by_better_team = 0
+
     # 2D numpy array: num_rows=trials, num_cols=games in series
     outcomes_array = np.random.random(size=(num_trials, num_games_in_series))
+
     for series in outcomes_array:
         num_games_won_by_better_team = np.count_nonzero(series < chance_better_team_wins)
         if num_games_won_by_better_team > (num_games_in_series - num_games_won_by_better_team):
             num_series_won_by_better_team += 1
+
     series_win_rate = num_series_won_by_better_team / float(num_trials)
     return series_win_rate
 
